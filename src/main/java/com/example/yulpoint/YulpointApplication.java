77689abd665a7d954d5eab060b63e53ad64c5c49
tamnao6 <2002worldcup@gmail.com>
2024-08-05 17:25:48 +0900

    Initial commit

---
+++ src/main/java/com/example/yulpoint/YulpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.yulpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YulpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YulpointApplication.class, args);
+    }
+
+}
